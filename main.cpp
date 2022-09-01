ec87a872b55de3318811657f5b7b0766eed874ca
swager253 <swager253@naver.com>
2022-09-01 12:58:39 +0900

    beakJoon 4659

--- main.cpp
+++ main.cpp
@@ -7,18 +7,17 @@ int cnt[30] = {0,};
 
 bool is_valid(int idx) {
 
-    return (idx == ('a' - 'a') || idx == ('e' - 'a') || idx == ('i' - 'a') || idx == ('o' - 'a') || idx == ('u' - 'a'));
+    return (idx == 0 || idx == 4 || idx == 8 || idx == 14 || idx == 20);
 }
 int main() {
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    int a_var = 0, b_var = 0;
-
     string str;
 
     while (1) {
+        int a_var = 0, b_var = 0;
         int flag = 0;
         cin >> str;
 
@@ -34,8 +33,14 @@ int main() {
             else b_var++, a_var = 0;
 
             if(a_var == 3 || b_var == 3) flag = 1;
-            
+
+            if(i >= 1 && (str[i - 1] == str[i]) && (idx != 4 && idx != 14)) flag = 1;
+
         }
+        if(!cnt[0] && !cnt[4] && !cnt[8] && !cnt[14] && !cnt[20]) flag = 1;
+
+        if(flag) cout << "<" << str << ">" << " is not acceptable." << "\n";
+        else cout << "<" << str << ">" << " is acceptable." << "\n";
     }
 
     return 0;
